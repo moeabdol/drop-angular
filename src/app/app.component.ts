13bd62de01aac4f05e37a11c3ab19b2c365f044c
moeabdol <mohd.a.saed@gmail.com>
2017-11-14 03:19:37 +0300

    Implement dropzone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var $: any;
+declare var Dropzone: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    Dropzone.options.imagesDropzone = {
+      autoProcessQueue: false,
+      url: 'http://localhost:3000/api/uploads',
+      paramName: 'image',
+
+      init: function() {
+        const submitButton = $('#submit-all');
+        const imagesDropzone = this;
+
+        submitButton.on('click', () => {
+          imagesDropzone.options.autoProcessQueue = true;
+          imagesDropzone.processQueue();
+        });
+
+        imagesDropzone.on('queuecomplete', () => {
+          imagesDropzone.options.autoProcessQueue = false;
+        });
+      }
+    };
+  }
 }
